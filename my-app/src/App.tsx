c1ac5c8c1629fba26df5f661a022fcba2a9f42a3
William Koerich <williamkoerich@hotmail.com>
2021-07-30 23:25:07 -0300

    created front end challenge

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import './App.css'
+import api from './utils/api'
 
 function App() {
+  const [link, setLink] = useState<string>('')
+  const [shortLink, setShortLink] = useState<string>('')
+
+  const realLink = (event: ChangeEvent<HTMLInputElement>) => {
+    setLink(event.target.value)
+  }
+
+  const saveLink = async () => {
+    await api
+      .post(`/link/`, { link: `'${link}'` })
+      .then(res => setShortLink(res.data.shortLink))
+  }
+
+  const redirect = () => {
+    return window.open(`${link}`)
+  }
+
   return (
     <div className="App">
       <div className="title">Short Link</div>
+      <input type="text" placeholder="Cole seu link aqui" onChange={realLink} />
+      <button onClick={saveLink}>Aplicar</button>
+      <button onClick={redirect}>Redirecionar</button>
+      <div>{shortLink}</div>
     </div>
   )
 }
